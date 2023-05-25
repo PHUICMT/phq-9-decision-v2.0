be73cf870f1aa465d9dd5ee671283bb7794c295b
PHUICMT <icmtchannel@gmail.com>
2023-05-25 20:22:29 +0700

    update model

--- decision.py
+++ decision.py
@@ -1,6 +1,6 @@
 import pickle
 
-clf = pickle.load(open("LableDecisionTreeModel.pkl", "rb"))
+clf = pickle.load(open("LableDecisionTreeModel_New.pkl", "rb"))
 
 def predict(data):
     try: 
@@ -17,11 +17,9 @@ def predict(data):
         return "Error"
 
 def initialize_data(df):
-    features = ["Type", "Score" , "Change", "Skip", "Overtime", "Submit", "Worry", "Sad", "Happy", "No Emotion"]
-    d_type = {'Depression': 0, 'Normal': 1, 'treating': 2}
+    features = ["Score" , "Change", "Skip", "Overtime", "Submit", "Worry", "Sad", "Happy", "Natural"]
     d_behavior = {'Y': 1, 'N': 0}
 
-    df["Type"] = df["Type"].map(d_type)
     df["Change"] = df["Change"].map(d_behavior)
     df["Skip"] = df["Skip"].map(d_behavior)
     df["Overtime"] = df["Overtime"].map(d_behavior)
